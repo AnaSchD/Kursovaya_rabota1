b71ebbbd05b60f41838ddbab23dc4223e005dcb3
Scherbakova <nastyasch0806@gmail.com>
2022-09-05 21:44:27 +0300

    второй коммит

---
+++ src/Employee.java
@@ -0,0 +1,21 @@
+public class Employee {
+
+    private String name;
+    private String surname;
+    private String patronymic;
+    private int departmentNumber;
+    private double salary;
+
+    public Employee(String name, String surname, String patronymic, int departmentNumber, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.departmentNumber = departmentNumber;
+        this.salary = salary;
+    }
+
+
+
+
+
+}
